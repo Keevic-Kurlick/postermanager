37f7aca730cf5d591e7bb65a5d59abe1817bb870
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-03-16 23:24:35 +0300

    film class

---
+++ src/main/java/ru/netology/poster/Film.java
@@ -0,0 +1,51 @@
+package ru.netology.poster;
+
+public class Film {
+    private int id;
+    private String name;
+    private String genre;
+    boolean isPremiere;
+
+    public Film(int id, String name, String genre, boolean isPremiere) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.isPremiere = isPremiere;
+    }
+
+    public String getFilm() {
+        return "(" + id + ") " + name + " [" + genre + "] " + "Премьера: " + isPremiere;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId(int id) {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isPremiere() {
+        return isPremiere;
+    }
+
+    public void setPremiere(boolean premiere) {
+        isPremiere = premiere;
+    }
+}
